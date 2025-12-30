b52abc72c5b2a84b7b18e0d2fe26471f3cd74f5b
SotaTamura <tamso1868@gmail.com>
2025-12-30 18:42:46 +0900

    init4

--- lib/main.dart
+++ lib/main.dart
@@ -29,63 +29,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late final MapController _mapController;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+  }
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    // 本体
     return Scaffold(
+      // ヘッダー
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
+      // マップ
+      body: FlutterMap(
+        mapController: _mapController,
+        options: const MapOptions(
+          initialCenter: LatLng(51.509364, -0.128928),
+          initialZoom: 9.2,
+          interactionOptions: InteractionOptions(flags: InteractiveFlag.all),
+        ),
         children: [
-          Expanded(
-            child: FlutterMap(
-              mapController: MapController(),
-              options: MapOptions(
-                initialCenter: const LatLng(
-                  51.509364,
-                  -0.128928,
-                ), // Center the map over London, UK
-                initialZoom: 9.2,
-                interactionOptions: const InteractionOptions(
-                  flags: InteractiveFlag.doubleTapDragZoom,
-                ),
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+          ),
+          // ピン
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(35.65961, 139.68303),
+                width: 60,
+                height: 60,
+                alignment: Alignment.centerLeft,
+                child: Icon(Icons.location_pin, size: 60, color: Colors.red),
               ),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'memomap',
-                ),
-                RichAttributionWidget(
-                  attributions: [
-                    TextSourceAttribution(
-                      'OpenStreetMap contributors',
-                      onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            ],
+          ),
+          // クレジット
+          RichAttributionWidget(
+            alignment: AttributionAlignment.bottomLeft,
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+                onTap: () =>
+                    launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+              ),
+            ],
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Text(
-          '$_counter',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
+      // 操作ボタン
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _mapController.move(
+              _mapController.camera.center,
+              _mapController.camera.zoom + 1,
+            ),
+            tooltip: 'Zoom in',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            onPressed: () => _mapController.move(
+              _mapController.camera.center,
+              _mapController.camera.zoom - 1,
+            ),
+            tooltip: 'Zoom out',
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            onPressed: () =>
+                _mapController.move(const LatLng(35.6895, 139.6917), 10),
+            tooltip: 'Move to Tokyo',
+            child: const Icon(Icons.location_city),
+          ),
+        ],
       ),
     );
   }
