53c4b340e7f2e890c7080ccf15a8c9249d31c524
SotaTamura <tamso1868@gmail.com>
2025-12-30 13:06:52 +0900

    init3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Memomap',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.blue)),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.purple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -41,11 +44,40 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(mainAxisAlignment: .center, children: [
-
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FlutterMap(
+              mapController: MapController(),
+              options: MapOptions(
+                initialCenter: const LatLng(
+                  51.509364,
+                  -0.128928,
+                ), // Center the map over London, UK
+                initialZoom: 9.2,
+                interactionOptions: const InteractionOptions(
+                  flags: InteractiveFlag.doubleTapDragZoom,
+                ),
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'memomap',
+                ),
+                RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                      onTap: () => launchUrl(
+                        Uri.parse('https://openstreetmap.org/copyright'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
