1877139e41270ba66af96d8b9046fc4a5bdbf207
SotaTamura <tamso1868@gmail.com>
2026-02-26 19:32:57 +0900

    描画機能

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,5 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
 
-  runApp(
-    const ProviderScope(
-      child: App(),
-    ),
-  );
+  runApp(const ProviderScope(child: App()));
 }
